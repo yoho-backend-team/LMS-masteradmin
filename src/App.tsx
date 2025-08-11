7f3d987c97c199773ae7736990145d67577f2bed
Vignesh <smartvignesh81245@email.com>
2025-08-11 17:24:53 +0530

    api completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,44 @@
-import { BrowserRouter } from 'react-router-dom'
-import './App.css'
-import Approutes from './routes/Approutes'
-import { Toaster } from 'react-hot-toast'
-import { Provider } from 'react-redux'
-import { store } from './store/index'
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
+import Approutes from './routes/Approutes';
+import { Toaster } from 'react-hot-toast';
+import { Provider } from 'react-redux';
+import { store } from './store/index';
+
 
 function App() {
 
-
 	return (
-		<>
-			<BrowserRouter>
-				<Provider store={store}>
-					<Toaster
-						position='top-right'
-						toastOptions={{
-							duration: 4000,
+		<BrowserRouter>
+			<Provider store={store}>
+				<Toaster
+					position='top-right'
+					toastOptions={{
+						duration: 4000,
+						style: {
+							background: '#363636',
+							color: '#fff',
+						},
+						success: {
+							duration: 3000,
 							style: {
-								background: '#363636',
+								background: '#10B981',
 								color: '#fff',
 							},
-							success: {
-								duration: 3000,
-								style: {
-									background: '#10B981',
-									color: '#fff',
-								},
+						},
+						error: {
+							duration: 4000,
+							style: {
+								background: '#EF4444',
+								color: '#fff',
 							},
-							error: {
-								duration: 4000,
-								style: {
-									background: '#EF4444',
-									color: '#fff',
-								},
-							},
-						}}
-					/>
-					<Approutes />
-				</Provider>
-			</BrowserRouter>
-		</>
-	)
+						},
+					}}
+				/>
+				<Approutes />
+			</Provider>
+		</BrowserRouter>
+	);
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,38 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-   plugins: [react(), tailwindcss()],
+   plugins: [react(), tailwindcss(),
+     VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true, // Allows testing in dev mode
+      },
+      manifest: {
+        name: 'My App',
+        short_name: 'App',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
