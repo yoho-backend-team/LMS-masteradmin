929bc2f714b27bb6b5543760c0823dbc7b88a078
suruthiga407! <suruthiga.yaho@gmail.com>
2025-08-19 18:28:16 +0530

    papi

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.m9r736h1e6g"
+    "revision": "0.g8ggtblgf4g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
