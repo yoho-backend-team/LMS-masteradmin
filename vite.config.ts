a66dbd86fc6211814db3175b75cdcdfe407c560d
Raajes Kumar K S <raajeskumar.yoho@gmail.com>
2025-09-15 10:49:11 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from "path"
-import tailwindcss from "@tailwindcss/vite"
+
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-   plugins: [react(), tailwindcss(),
+   plugins: [react(),
      VitePWA({
       registerType: 'autoUpdate',
       devOptions: {
