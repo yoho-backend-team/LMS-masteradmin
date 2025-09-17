822d104815cdfaa0a9838762832d8ab4913bde36
suruthiga407! <suruthiga.yaho@gmail.com>
2025-09-17 17:16:42 +0530

    ...cha

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3n7iid6o708"
+    "revision": "0.r775ug1kvko"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
